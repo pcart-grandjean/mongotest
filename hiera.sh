11151c11d5ffc757d2d5334a59d1e4a3c4364dde
Pierre Cart-Grandjean <pcart-grandjean@amadeus.com>
2015-09-17 18:12:37 +0200

    Major progress made on debuging hierarchy with hiera command line

---
+++ hiera.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ ! $# -eq 1 ]
+then
+  echo "Please one argument: the key of the value you are looking for."
+  exit 1
+fi
+
+key=$1
+
+# hiera --config hiera.yaml --json facter.json --debug ${key} hostname=cubitus
+hiera --config hiera.yaml --json facts.json --debug ${key} hostname=cubitus
